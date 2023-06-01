42f2f0ba0505f38d210f1ef91a7a430ed81a1152
Julia <juliaretore@hotmail.com>
2023-06-01 08:22:50 -0300

    add Part II

--- parte_1.py
+++ parte_1.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def apply_kernel(img, kernel):
     sz = kernel.shape[0] // 2
-
+    
     # add mirror board
     img = cv2.copyMakeBorder(img, sz, sz, sz, sz, cv2.BORDER_REFLECT)
 
@@ -22,7 +22,7 @@ def question_i():
     img = cv2.imread('imagens/entrada/Lua1_gray.jpg', cv2.IMREAD_GRAYSCALE)
 
     mean_kernel_3 = np.full((3, 3), 1 / 9)
-    mean_kernel_5 = np.full((5, 5), 1 / 9)
+    mean_kernel_5 = np.full((5, 5), 1 / 25)
 
     img_3 = apply_kernel(img, mean_kernel_3)
     img_5 = apply_kernel(img, mean_kernel_5)
@@ -35,29 +35,30 @@ def question_ii():
     img = cv2.imread('imagens/entrada/Lua1_gray.jpg', cv2.IMREAD_GRAYSCALE)
 
     def create_gaussian_kernel(size, sigma):
+        sz = size // 2
         gaussian_kernel = np.zeros((size, size))
-        for i in range(3):
-            for j in range(3):
-                x = i - 1
-                y = j - 1
+        for i in range(size):
+            for j in range(size):
+                x = i - sz
+                y = j - sz
                 gaussian_kernel[i, j] = (1 / (2 * np.pi * sigma ** 2)) * np.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
+
         return gaussian_kernel
 
     gaussian_kernel_06 = create_gaussian_kernel(3, 0.6)
-    gaussian_kernel_10 = create_gaussian_kernel(3, 1.0)
+    gaussian_kernel_10_a = create_gaussian_kernel(3, 1.0)
+    gaussian_kernel_10_b = create_gaussian_kernel(7, 1.0)
 
     img_06 = apply_kernel(img, gaussian_kernel_06)
-    img_10 = apply_kernel(img, gaussian_kernel_10)
+    img_10_a = apply_kernel(img, gaussian_kernel_10_a)
+    img_10_b = apply_kernel(img, gaussian_kernel_10_b)
 
     cv2.imwrite('imagens/saida/1_b_06.png', img_06)
-    cv2.imwrite('imagens/saida/1_b_10.png', img_10)
+    cv2.imwrite('imagens/saida/1_b_10_a.png', img_10_a)
+    cv2.imwrite('imagens/saida/1_b_10_b.png', img_10_b)
 
-
-def question_iii():
-    pass
 
 
 if __name__ == '__main__':
     question_i()
     question_ii()
-    question_iii()
---
+++ parte_2.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+def question_i():
+    img = cv2.imread('imagens/entrada/Lua1_gray.jpg', cv2.IMREAD_GRAYSCALE)
+
+    a = np.array([[0,  1, 0],
+                  [1, -4, 1],
+                  [0,  1, 0]])
+    
+    
+    b = np.array([[1,  1, 1],
+                  [1, -8, 1],
+                  [1,  1, 1]])
+    
+    c = -1
+
+    laplacian_image_a =  cv2.filter2D(src=img, ddepth=-1, kernel=a)
+    laplacian_image_b =  cv2.filter2D(src=img, ddepth=-1, kernel=a)
+
+    img_a = img + c * laplacian_image_a
+    img_b = img + c * laplacian_image_b
+
+    cv2.imwrite('imagens/saida/2_a.png', img_a)
+    cv2.imwrite('imagens/saida/2_b.png', img_b)
+
+
+if __name__ == '__main__':
+    question_i()
