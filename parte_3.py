21c0f6e4c29a2e85cd11cf881cf83f93f21731e2
EMachad0 <elitonmachadod200@gmail.com>
2023-06-02 22:41:36 -0300

    parte 3

--- parte_3.py
+++ parte_3.py
@@ -2,11 +2,11 @@ import cv2
 import math
 import numpy as np
 
-Pwx = np.array([ [-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+Pwx = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
 Pwy = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
 
 Sbx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-Sby = np.array([[-1, -2, -1], [0, 0, 0], [1, 2 ,1] ])
+Sby = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
 
 Scx = np.array([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]])
 Scy = np.array([[-3, -10, -3], [0, 0, 0], [3, 10, 3]])
@@ -30,30 +30,81 @@ def apply_kernel(img, kernel):
     output_img = output_img[sz:-sz, sz:-sz]
 
     # Normalize
-    output_img = np.clip(output_img, 0, 255)
+    # output_img = np.abs(output_img)
+    output_img = (output_img - np.min(output_img)) / (np.max(output_img) - np.min(output_img)) * 255
 
     return output_img.astype(np.uint8)
 
 
-def gradient_magnitude(img, Gx, Gy):
-    Gx = Gx.astype(np.float32)
-    Gy = Gy.astype(np.float32)
-    magnitude = np.sqrt(Gx ** 2 + Gy ** 2)
-    magnitude = np.clip(magnitude, 0, 255)
-    magnitude = magnitude.astype(np.uint8)
+def gradient_magnitude(gx, gy):
+    gx = gx.astype(np.int32)
+    gy = gy.astype(np.int32)
+    magnitude = np.around(np.sqrt(gx ** 2 + gy ** 2))
+    magnitude = magnitude / np.max(magnitude) * 255
 
-    return magnitude
+    return magnitude.astype(np.uint8)
 
 
-def gradient_direction(img, Gx, Gy):
-    Gx = Gx.astype(np.float32)
-    Gy = Gy.astype(np.float32)
-    eps = 10 ** (-8) 
-    direction = np.degrees(np.arctan2(Gy, Gx + eps))
-    direction = np.clip(direction, 0, 255)    
-    direction = direction.astype(np.uint8)
+def gradient_direction(gx, gy):
+    gx = gx.astype(np.int32)
+    gy = gy.astype(np.int32)
+    eps = 10 ** (-8)
+    direction = np.degrees(np.arctan2(gy, gx + eps))
+    direction = (direction - np.min(direction)) / (np.max(direction) - np.min(direction)) * 255
 
-    return direction
+    return direction.astype(np.uint8)
+
+
+def local_maximum(gx, gy):
+    gx = gx.astype(np.int32)
+    gy = gy.astype(np.int32)
+    magnitude = np.around(np.sqrt(gx ** 2 + gy ** 2))
+    eps = 10 ** (-8)
+    direction = np.degrees(np.arctan2(gy, gx + eps))
+
+    def get_neighbours(i, j):
+        mapping = {
+            'A': (i - 1, j - 1),
+            'B': (i - 1, j),
+            'C': (i - 1, j + 1),
+            'D': (i, j - 1),
+            'F': (i, j + 1),
+            'G': (i + 1, j - 1),
+            'H': (i + 1, j),
+            'I': (i + 1, j + 1)
+        }
+        if 22.5 < direction[i, j] <= 67.5:
+            return mapping['C'], mapping['G']
+        elif 67.5 < direction[i, j] <= 112.5:
+            return mapping['B'], mapping['H']
+        elif 112.5 < direction[i, j] <= 157.5:
+            return mapping['A'], mapping['I']
+        elif 157.5 < direction[i, j] <= 180:
+            return mapping['D'], mapping['F']
+        elif -22.5 < direction[i, j] <= 22.5:
+            return mapping['F'], mapping['D']
+        elif -67.5 < direction[i, j] <= -22.5:
+            return mapping['I'], mapping['A']
+        elif -112.5 < direction[i, j] <= -67.5:
+            return mapping['H'], mapping['B']
+        elif -157.5 < direction[i, j] <= -112.5:
+            return mapping['G'], mapping['C']
+        elif -180 <= direction[i, j] <= -157.5:
+            return mapping['D'], mapping['F']
+
+    output_img = np.zeros_like(magnitude, dtype=np.int32)
+
+    for i in range(1, direction.shape[0] - 1):
+        for j in range(1, direction.shape[1] - 1):
+            y, x = get_neighbours(i, j)
+            yi, yj = y
+            xi, xj = x
+            if magnitude[i, j] > magnitude[yi, yj] and magnitude[i, j] > magnitude[xi, xj]:
+                output_img[i, j] = magnitude[i, j]
+
+    output_img = output_img / np.max(output_img) * 255
+    return output_img.astype(np.uint8)
+
 
 def vertical_components(img):
     pw = apply_kernel(img, Pwx)
@@ -62,6 +113,7 @@ def vertical_components(img):
 
     return pw, sb, sc
 
+
 def horizontal_components(img):
     pw = apply_kernel(img, Pwy)
     sb = apply_kernel(img, Sby)
@@ -69,32 +121,41 @@ def horizontal_components(img):
 
     return pw, sb, sc
 
+
 def create_imgs(img, name):
-    Gx_pw, Gx_sb, Gx_sc = vertical_components(img)
-    Gy_pw, Gy_sb, Gy_sc = horizontal_components(img)
+    gx_pw, gx_sb, gx_sc = vertical_components(img)
+    gy_pw, gy_sb, gy_sc = horizontal_components(img)
 
-    img_magnitude_pw = gradient_magnitude(img, Gx_pw, Gy_pw)
-    img_magnitude_sb = gradient_magnitude(img, Gx_sb, Gy_sb)
-    img_magnitude_sc = gradient_magnitude(img, Gx_sc, Gy_sc)
+    img_magnitude_pw = gradient_magnitude(gx_pw, gy_pw)
+    img_magnitude_sb = gradient_magnitude(gx_sb, gy_sb)
+    img_magnitude_sc = gradient_magnitude(gx_sc, gy_sc)
 
-    img_direction_pw = gradient_direction(img, Gx_pw, Gy_pw)
-    img_direction_sb = gradient_direction(img, Gx_sb, Gy_sb)
-    img_direction_sc = gradient_direction(img, Gx_sc, Gy_sc)
+    img_direction_pw = gradient_direction(gx_pw, gy_pw)
+    img_direction_sb = gradient_direction(gx_sb, gy_sb)
+    img_direction_sc = gradient_direction(gx_sc, gy_sc)
 
-    cv2.imwrite('imagens/saida/Prewitt/'+name+'_Gx.png', Gx_pw)
-    cv2.imwrite('imagens/saida/Prewitt/'+name+'_Gy.png', Gy_pw)
-    cv2.imwrite('imagens/saida/Prewitt/'+name+'_magnitude.png', img_magnitude_pw)
-    cv2.imwrite('imagens/saida/Prewitt/'+name+'_direction.png', img_direction_pw)
+    img_local_max_pw = local_maximum(gx_pw, gy_pw)
+    img_local_max_sb = local_maximum(gx_sb, gy_sb)
+    img_local_max_sc = local_maximum(gx_sc, gy_sc)
 
-    cv2.imwrite('imagens/saida/Sobel/'+name+'_Gx.png', Gx_sb)
-    cv2.imwrite('imagens/saida/Sobel/'+name+'_Gy.png', Gy_sb)
-    cv2.imwrite('imagens/saida/Sobel/'+name+'_magnitude.png', img_magnitude_sb)
-    cv2.imwrite('imagens/saida/Sobel/'+name+'_direction.png', img_direction_sb)
+    cv2.imwrite('imagens/saida/Prewitt/' + name + '_Gx.png', gx_pw)
+    cv2.imwrite('imagens/saida/Prewitt/' + name + '_Gy.png', gy_pw)
+    cv2.imwrite('imagens/saida/Prewitt/' + name + '_magnitude.png', img_magnitude_pw)
+    cv2.imwrite('imagens/saida/Prewitt/' + name + '_direction.png', img_direction_pw)
+    cv2.imwrite('imagens/saida/Prewitt/' + name + '_local_max.png', img_local_max_pw)
 
-    cv2.imwrite('imagens/saida/Scharr/'+name+'_Gx.png', Gx_sc)
-    cv2.imwrite('imagens/saida/Scharr/'+name+'_Gy.png', Gy_sc)
-    cv2.imwrite('imagens/saida/Scharr/'+name+'_magnitude.png', img_magnitude_sc)
-    cv2.imwrite('imagens/saida/Scharr/'+name+'_direction.png', img_direction_sc)
+    cv2.imwrite('imagens/saida/Sobel/' + name + '_Gx.png', gx_sb)
+    cv2.imwrite('imagens/saida/Sobel/' + name + '_Gy.png', gy_sb)
+    cv2.imwrite('imagens/saida/Sobel/' + name + '_magnitude.png', img_magnitude_sb)
+    cv2.imwrite('imagens/saida/Sobel/' + name + '_direction.png', img_direction_sb)
+    cv2.imwrite('imagens/saida/Sobel/' + name + '_local_max.png', img_local_max_sb)
+
+    cv2.imwrite('imagens/saida/Scharr/' + name + '_Gx.png', gx_sc)
+    cv2.imwrite('imagens/saida/Scharr/' + name + '_Gy.png', gy_sc)
+    cv2.imwrite('imagens/saida/Scharr/' + name + '_magnitude.png', img_magnitude_sc)
+    cv2.imwrite('imagens/saida/Scharr/' + name + '_direction.png', img_direction_sc)
+    cv2.imwrite('imagens/saida/Scharr/' + name + '_local_max.png', img_local_max_sc)
+
 
 if __name__ == '__main__':
     img_a = cv2.imread('imagens/saida/1_a_3.png', cv2.IMREAD_GRAYSCALE)
